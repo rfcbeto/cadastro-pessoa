f8dfd1361fd39610c304eec3dd7f44a40c90c24c
rfcbeto <rfcbeto@gmail.com>
2017-09-02 21:48:37 -0300

    Inclusão do projeto.

---
+++ src/main/java/br/com/siscad/cadpessoa/CadpessoaApplication.java
@@ -0,0 +1,12 @@
+package br.com.siscad.cadpessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoaApplication.class, args);
+	}
+}
